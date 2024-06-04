df2e66c0b28271fbd9a2d88a4c036ed2d9555200
AlexGioffDev <alex.gioffre_dev@outlook.it>
2024-06-04 10:40:45 +0200

    TemperatureConverter: Init

---
+++ TemperatureConverter/TemperatureConverter/TemperatureConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureConverterApp.swift
+//  TemperatureConverter
+//
+//  Created by Alex Gioffre' on 04/06/24.
+//
+
+import SwiftUI
+
+@main
+struct TemperatureConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     @State private var checkAmount = 0.0
     @State private var peoples = 2
+    @FocusState private var focused: Bool
     
     var totalPerson: Double {
         // Calculate
@@ -21,7 +22,7 @@ struct ContentView: View {
         NavigationStack{
             Form {
                 Section {
-                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "EUR")).keyboardType(.decimalPad)
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "EUR")).keyboardType(.decimalPad).focused($focused)
                     Picker("Number of people", selection: $peoples) {
                         ForEach(2..<11){
                             Text("\($0) peoples")
@@ -34,6 +35,13 @@ struct ContentView: View {
             }
             .navigationTitle("We Split")
             .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                if focused {
+                    Button("Done") {
+                        focused = false
+                    }
+                }
+            }
             
            
         }
