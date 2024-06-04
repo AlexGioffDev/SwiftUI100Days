7eacc82dbd6e282ab59c6b4d2f38d795097aa12d
AlexGioffDev <alex.gioffre_dev@outlook.it>
2024-06-04 11:10:16 +0200

    TemperatureConverter: Done

--- TemperatureConverter/TemperatureConverter/ContentView.swift
+++ TemperatureConverter/TemperatureConverter/ContentView.swift
@@ -8,14 +8,70 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    var enterUnit = ["Celcius", "Fahrenheit", "Kelvin"]
+    
+    var outUnit = ["Celcius", "Fahrenheit", "Kelvin"]
+    
+    @State private var temperatureIn = 0.0
+    
+    @State private var selectedEnterUnit = "Celsius"
+    @State private var selectedOutUnit = "Fahrenheit"
+    
+    var outTemperature: Double {
+
+        let tempInCelsius: Double
+                
+        // Si converte se necessario la temperature in entrata in celcius
+        switch selectedEnterUnit {
+            case "Fahrenheit":
+                tempInCelsius = (temperatureIn - 32) * 5 / 9
+            case "Kelvin":
+                tempInCelsius = temperatureIn - 273.15
+            default:
+                tempInCelsius = temperatureIn
         }
-        .padding()
+                
+        // Si ritorna il valore in base l'unità scelta
+        switch selectedOutUnit {
+            case "Fahrenheit":
+                return tempInCelsius * 9 / 5 + 32
+            case "Kelvin":
+                return tempInCelsius + 273.15
+            default:
+                return tempInCelsius
+        }
+    }
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section("Temperature in: \(selectedEnterUnit)") {
+                    TextField("Temperature: ", value: $temperatureIn, format: .number).keyboardType(.decimalPad)
+                }
+                
+                Section("Choise your unit!") {
+                    Picker("In Temperature", selection: $selectedEnterUnit){
+                        ForEach(enterUnit, id: \.self){
+                            Text($0)
+                        }
+                    }.pickerStyle(.segmented)
+                    Picker("Out Temperature", selection: $selectedOutUnit){
+                        ForEach(outUnit, id: \.self){
+                            Text($0)
+                        }
+                    }.pickerStyle(.segmented)
+                }
+                
+                Section{
+                    EmptyView()
+                }
+                
+                Section("Temperature in: \(selectedOutUnit)") {
+                    Text(outTemperature, format: .number)
+                }
+            }
+            .navigationTitle("TemperatureConverter")
+            .navigationBarTitleDisplayMode(.inline)
+        }
     }
 }
 
