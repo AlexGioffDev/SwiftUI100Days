19ddcd5c479d36c989d4a0e171fb2f566b9f8a89
AlexGioffDev <alex.gioffre_dev@outlook.it>
2024-06-25 16:57:23 +0200

    CupcakeCorner: Detail Order

---
+++ CupcakeCorner/CupcakeCorner/AdressView.swift
@@ -0,0 +1,19 @@
+//
+//  AdressView.swift
+//  CupcakeCorner
+//
+//  Created by Alex Gioffre' on 25/06/24.
+//
+
+import SwiftUI
+
+struct AdressView: View {
+    var order: Order
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AdressView(order: Order())
+}
