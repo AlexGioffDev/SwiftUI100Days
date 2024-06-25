85807f49db9f161825ca34422126b53212dce971
AlexGioffDev <alex.gioffre_dev@outlook.it>
2024-06-25 09:26:23 +0200

    CupcakeCorner: Init

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Alex Gioffre' on 25/06/24.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
