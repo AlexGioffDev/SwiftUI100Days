97ee32db1b8cbef4fe49ab74765db8984864db70
AlexGioffDev <alex.gioffre_dev@outlook.it>
2024-06-26 09:23:50 +0200

    BookWorm: init

---
+++ BookWorm/BookWorm/BookWormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormApp.swift
+//  BookWorm
+//
+//  Created by Alex Gioffre' on 26/06/24.
+//
+
+import SwiftUI
+
+@main
+struct BookWormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
